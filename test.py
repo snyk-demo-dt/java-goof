7b8d33208700c797b010aa8ce87ee719cdd7ddb7
shawnsnyk <38260497+shawnsnyk@users.noreply.github.com>
2024-07-29 18:26:43 -0400

    Update and rename test.py to test3.py

--- test.py
+++
@@ -1,3 +0,0 @@
-#work in progress - example for nightfall
-mydb="postgresql://logger_service:U34Fx43343g!b22w@cloud.google.com:5432/capture"
-password="myPassTxt1234!"
---
+++ test3.py
@@ -0,0 +1,3 @@
+#work in progress - example for nightfall
+mydb="postgresql://logger_service:U34Fx4233222343g!b22w@cloud.google.com:5132/capture"
+password="myPassafasf342343234!"
