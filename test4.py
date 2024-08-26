975e6afd0ca666cc426732d0b264ae897dabdd8e
shawnsnyk <38260497+shawnsnyk@users.noreply.github.com>
2024-08-26 15:06:08 -0400

    Update and rename test4.py to test5.py

--- test4.py
+++
@@ -1,3 +0,0 @@
-#work in progress - example for nightfall
-mydb="postgresql://logger_service:U34Fx42434322343g!b22w@cloud.google.com:5132/capture"
-password="myPassafasf367234!"
---
+++ test5.py
@@ -0,0 +1,3 @@
+#work in progress - example for nightfall
+mydb="postgresql://logger_service:U34Fx4878722343g!b22w@cloud.google.com:5132/capture"
+password="myPassafasf98234!"
